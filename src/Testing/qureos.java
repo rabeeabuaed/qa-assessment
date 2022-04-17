c3df96286c2687be91b9a38d92ea186471a07f43
rabeeabuaed <rabeeabuaed@gmail.com>
2022-04-18 00:22:50 +0400

    end of task 1

--- src/Testing/qureos.java
+++ src/Testing/qureos.java
@@ -15,25 +15,32 @@ public class qureos {
 	public static void login(WebDriver driver, String baseUrl, String driverPath) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		driver = new ChromeDriver();
+		// go to "https://app.qureos.com/" website
 		driver.get(baseUrl);
+		// define username element and throw the username
 		driver.findElement(
-				By.xpath("//*[@id=\"__next\"]/main/section/div/section/section/div/div/form/section[1]/input"))
+				By.xpath("//*[@id=\"__next\"]/main/section/div/section/section/div/div/form/section[1]/input")) 
 				.sendKeys("rabeeabuaed@gmail.com");
+		 //define Pass element and throw the password
 		driver.findElement(By.cssSelector(
 				"#__next > main > section > div > section > section > div > div > form > section:nth-child(2) > input"))
 				.sendKeys("Rabee_1992n");
-		driver.findElement(
+		 //define login button 
+			driver.findElement(
 				By.xpath("//*[@id=\"__next\"]/main/section/div/section/section/div/div/form/div/section/button"))
 				.click();
+		// sleep the page for 3 second
 		Thread.sleep(3000);
+		// make sure the login is success
 		Assert.assertEquals("https://app.qureos.com/home?refresh=1", driver.getCurrentUrl());
 		Thread.sleep(3000);
-
+		// close the browser
 		driver.quit();
 	}
-
+	// main function
 	public static void main(String[] args) throws InterruptedException {
 		try {
+			// call the function
 			login(driver, baseUrl, driverPath);
 
 		} catch (Exception e) {
